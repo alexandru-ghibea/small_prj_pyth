282a0c2fce4012bf8738072f5c34deca93f6f9fc
alexandru-ghibea <98416718+alexandru-ghibea@users.noreply.github.com>
2022-04-01 18:58:42 +0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,12 @@
 """
-Write a program that works out whether if a given year is a leap year.
+A program that works out whether if a given year is a leap year.
 A normal year has 365 days, leap years have 366, with an extra day in February.
-The reason why we have leap years is really fascinating, this video does it more justice:
-https://www.youtube.com/watch?v=xX96xng7sAE
+The reason why we have leap years is really fascinating, this video does it more justice: https://www.youtube.com/watch?v=xX96xng7sAE
+
 This is how you work out whether if a particular year is a leap year:
-on every year that is evenly divisible by 4
-**except** every year that is evenly divisible by 100
-**unless** the year is also evenly divisible by 400
+- on every year that is evenly divisible by 4
+- **except** every year that is evenly divisible by 100
+- **unless** the year is also evenly divisible by 400
 """
 
 year = int(input("Please provide the year to check if it's a leap year or not: "))
