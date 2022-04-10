ffc2f4a9c27da27b95b69497d5e9bb66df4faaa4
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-04-10 19:16:13 +0300

    Update guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,18 +1,16 @@
-"""
-Guess a random word
-"""
-
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
 display = []
 for letter in chosen_word:
     display.append("_")
 print(display)
-no_of_guesses = len(chosen_word)
+no_of_guesses = len(chosen_word) + 1
 game_on = True
 while game_on:
+    print("Number of guesses remaining:", no_of_guesses)
     guess = input("Guess a letter: ").lower()
     index = 0
     for letter2 in chosen_word:
@@ -20,13 +18,12 @@ while game_on:
             display[index] = letter2
         index += 1
     no_of_guesses -= 1
-    print("Number of guesses remaining:", no_of_guesses)
+
     print(display)
     if no_of_guesses < 0:
         print("You loose.Word was:", chosen_word)
         game_on = False
+        break
     if "".join(display) == chosen_word:
         print("You won. Word was:", chosen_word)
         game_on = False
-
-
