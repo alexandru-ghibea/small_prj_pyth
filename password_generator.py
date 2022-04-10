82a07d9c1b10f0594da34448f38a7e240ba21338
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-04-10 11:43:40 +0300

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,30 @@
+"""
+Create a program that generates a random password for
+ 1. n specified letters e.g. 13 letter password
+ 2. n specified symbols e.g. @#$
+ 3. n specified numbers e.g. 1/2/3
+"""
+import random
+import string
+
+all_letters = string.ascii_letters
+numbers = string.digits
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print("Welcome to the PyPassword Generator!")
+number_of_letters = int(input("How many letters would you like in your password ?: "))
+number_of_symbols = int(input("How many symbols would you like?: "))
+number_of_numbers = int(input("How many numbers would you like?: "))
+
+letters_to_use = ''.join(random.choice(string.ascii_letters) for x in range(number_of_letters))
+numbers_to_use = ''.join(random.choice(string.digits) for x in range(number_of_numbers))
+symbols_to_use = ''.join(random.choice(symbols) for x in range(number_of_symbols))
+password = (letters_to_use + numbers_to_use + symbols_to_use)
+print(f"Here is your password 1 version :{password}")
+
+"""
+For added complexity to the password you can shuffle the password.
+"""
+
+password2 = "".join(random.sample(password,len(password)))
+print(f"Here is your password 2 version :{password2}")
