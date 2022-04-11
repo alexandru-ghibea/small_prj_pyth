61ef6628e41033292a8368b538c2519b1c180c47
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-04-11 09:35:45 +0300

    Update guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,7 +2,6 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(f'Pssst, the solution is {chosen_word}.')
 display = []
 for letter in chosen_word:
     display.append("_")
