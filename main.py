2f6d1abc23a1d942c4f9390e1d1e9fb16f49b1d6
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-06-17 16:47:32 +0300

    Add files via upload
    Get your name in the Nato Alphabet
    
    Enjoy!

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+new_dict = {row.letter: row.code for index, row in data.iterrows()}
+print(new_dict)
+
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+nato_phonetic = [new_dict[letter] for letter in word]
+print(nato_phonetic)
